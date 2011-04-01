dc3518f44cf78e08393463fdeaed22a98a63c1ab
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2011-04-02 01:17:12 +0400

    More configure flags to avoid picking up host libraries.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ PATH=$ROOT/bin:$ROOT/nacl64/usr/bin:$PATH
 
 export SDL_LIBS="-L$ROOT/nacl64/usr/lib -lSDL -lppapi_cpp -lpthread -lm"
 CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
-  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --disable-music-flac
+  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --disable-music-flac --disable-music-mod --disable-music-mp3
 make
 make install
 
