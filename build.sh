01cc2351af64e581c14c13131dd76bdf97c95510
Evgeniy Stepanov <eugenis@google.com>
2011-04-01 15:29:12 +0400

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -x
+
+ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
+PATH=$ROOT/bin:$ROOT/nacl64/usr/bin:$PATH
+
+export SDL_LIBS="-L$ROOT/nacl64/usr/lib -lSDL -lppapi_cpp -lpthread -lm"
+CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
+  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --disable-music-flac
+make
+make install
+
