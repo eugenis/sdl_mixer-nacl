43b21109bf2c8515eeed5beaf921ca2496b96a87
Evgeniy Stepanov <eugenis@google.com>
2011-09-10 22:36:06 +0400

    Update the arch prefix.

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 set -e -x
 
 ROOT=${NACL_TOOLCHAIN_ROOT:-$HOME/root/nacl-sdk}
-PATH=$ROOT/bin:$ROOT/nacl64/usr/bin:$PATH
+PATH=$ROOT/bin:$ROOT/x86_64-nacl/usr/bin:$PATH
 
-export SDL_LIBS="-L$ROOT/nacl64/usr/lib -lSDL -lppapi_cpp -lpthread -lm"
-CC=nacl64-gcc AR=nacl64-ar RANLIB=nacl64-ranlib PKG_CONFIG_LIBDIR=$ROOT/nacl64/usr/lib/pkgconfig \
+export SDL_LIBS="-L$ROOT/x86_64-nacl/usr/lib -lSDL -lppapi_cpp -lpthread -lm"
+CC=x86_64-nacl-gcc AR=x86_64-nacl-ar RANLIB=x86_64-nacl-ranlib PKG_CONFIG_LIBDIR=$ROOT/x86_64-nacl/usr/lib/pkgconfig \
   LIBS="-lvorbisfile -lvorbis -logg -lm" \
-  ./configure --host=nacl64 --prefix=$ROOT/nacl64/usr --disable-music-flac --disable-music-mod --disable-music-mp3
+  ./configure --host=x86_64-nacl --prefix=$ROOT/x86_64-nacl/usr --disable-music-flac --disable-music-mod --disable-music-mp3
 make
 make install
 
